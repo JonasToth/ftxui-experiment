96534f32042a5c3bb9387c66f2f528fe188ebe70
Jonas Toth <development@jonas-toth.eu>
2023-05-12 20:57:15 +0200

    test ftx program that views a graph and a view properties

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,97 @@
+#include <atomic>
+#include <cmath>
 #include <iostream>
+#include <thread>
 
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/screen.hpp"
 #include "ftxui/screen/string.hpp"
 
-int main(void) {
+int main() {
+  using namespace std;
   using namespace ftxui;
 
-  auto summary = [&] {
-    auto content = vbox({
-        hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-        hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-        hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-    });
-    return window(text(L" Summary "), content);
+  auto screen = ScreenInteractive::Fullscreen();
+
+  int shift = 0;
+
+  auto my_graph = [&shift](int width, int height) {
+    std::vector<int> output(width);
+    for (int i = 0; i < width; ++i) {
+      float v = 0.5f;
+      v += 0.1f * sin((i + shift) * 0.1f);
+      v += 0.2f * sin((i + shift + 10) * 0.15f);
+      v += 0.1f * sin((i + shift) * 0.03f);
+      v *= height;
+      output[i] = (int)v;
+    }
+    return output;
   };
 
-  auto document =  //
-      vbox({
-          hbox({
-              summary(),
-              summary(),
-              summary() | flex,
-          }),
-          summary(),
-          summary(),
-      });
+  auto htop = Renderer([&]() -> Element {
+    return vbox({
+        text("Frequency [Mhz]") | hcenter,
+        hbox({
+            vbox({
+                text("2400 "),
+                filler(),
+                text("1200 "),
+                filler(),
+                text("0 "),
+            }),
+            graph(std::ref(my_graph)) | flex,
+        }) | flex,
+    });
+  });
 
-  // Limit the size of the document to 80 char.
-  document = document | size(WIDTH, LESS_THAN, 80);
+  auto singleSlot1 =
+      hbox({vbox({text("Slot 1"), text("EntityID: 42")}), separatorLight(),
+            hbox({color(Color::Red, text("G") | bold),
+                  color(Color::White, text("V") | bold),
+                  color(Color::Green, text("F") | bold)}),
+            separatorLight(),
+            htop->Render() | flex | size(WIDTH, GREATER_THAN, 80) | size(HEIGHT, GREATER_THAN, 15)});
+  auto singleSlot2 = hbox({text("Slot 2"), separatorLight(),
+                           hbox({bgcolor(Color::Red, text("G") | bold),
+                                 bgcolor(Color::Black, text("V") | bold),
+                                 bgcolor(Color::Green, text("F") | bold)})});
+  auto singleSlot3 = hbox({text("Slot 3"), separatorLight(),
+                           hbox({bgcolor(Color::Red, text("G") | bold),
+                                 bgcolor(Color::Black, text("V") | bold),
+                                 bgcolor(Color::Green, text("F") | bold)})});
+  auto singleSlot4 = hbox({text("Slot 4"), separatorLight(),
+                           hbox({bgcolor(Color::Red, text("G") | bold),
+                                 bgcolor(Color::Black, text("V") | bold),
+                                 bgcolor(Color::Green, text("F") | bold)})});
 
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  Render(screen, document);
+  auto slotDisplay = vbox({singleSlot1, separator(), singleSlot2, separator(),
+                           singleSlot3, separator(), singleSlot4});
 
-  std::cout << screen.ToString() << '\0' << std::endl;
+  auto main_renderer = Renderer([&] {
+    return vbox({text("FTXUI Demo") | bold | hcenter,
+                 hbox({slotDisplay | borderLight})});
+  });
+
+  std::atomic<bool> refresh_ui_continue{true};
+  std::thread refresh_ui([&] {
+    while (refresh_ui_continue) {
+      using namespace std::chrono_literals;
+      std::this_thread::sleep_for(0.05s);
+      // The |shift| variable belong to the main thread. `screen.Post(task)`
+      // will execute the update on the thread where |screen| lives (e.g. the
+      // main thread). Using `screen.Post(task)` is threadsafe.
+      screen.Post([&] { shift++; });
+      // After updating the state, request a new frame to be drawn. This is
+      // done by simulating a new "custom" event to be handled.
+      screen.Post(Event::Custom);
+    }
+  });
+
+  screen.Loop(main_renderer);
+  refresh_ui_continue = false;
+  refresh_ui.join();
 
   return EXIT_SUCCESS;
 }
